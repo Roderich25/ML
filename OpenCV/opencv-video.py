83d98589c6e6e33e1eb7e399cb482edde1568bd4
Roderich25 <rodrigoavilasolis@gmail.com>
2019-06-11 21:44:47 -0500

    opencv-video.py modified

--- OpenCV/opencv-video.py
+++ OpenCV/opencv-video.py
@@ -1,18 +1,20 @@
 import cv2
-
-cap = cv2.VideoCapture(0)
+import datetime
+cap = cv2.VideoCapture(0) # default camera
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 out = cv2.VideoWriter('output.avi', fourcc, 30.0, (640, 480))
 
 while(cap.isOpened()):
     ret, frame = cap.read()
     if ret == True:
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        dtt = str(datetime.datetime.now())
+        frame = cv2.putText(frame, dtt, (10, 50), font, 1, (0, 255, 255), 2, cv2.LINE_AA)
         out.write(frame)        
-        print(cap.get(cv2.CAP_PROP_FRAME_WIDTH), cap.get(cv2.CAP_PROP_FRAME_HEIGHT), cap.get(cv2.CAP_PROP_FPS))   
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        cv2.imshow('myFrame', gray)        
-        
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        print(cap.get(3), cap.get(4), cap.get(cv2.CAP_PROP_FPS)) # cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, fps
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.imshow('myFrame', frame)                
+        if cv2.waitKey(1) & 0xFF == ord('q'): # press q key to exit and save
             break
     else:
         break
